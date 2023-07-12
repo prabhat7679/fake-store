866ee780db3677919dc2e245493a12aff1b5aa8e
prabhat kumar <prabhat.kumar.24.2@mountblue.tech>
2023-07-12 21:15:54 +0530

    modified:   script.js wrote code to fetch api and display the data

---
+++ script.js
@@ -0,0 +1,25 @@
+
+fetch("https://fakestoreapi.com/products")
+  .then((data) =>{
+     return data.json(); 
+  })
+  .then((fetchData)=>{
+      console.log(fetchData)
+      let data1="";
+      fetchData.map((values)=>{
+
+        data1+=`<div class="card">
+        <h4 class="title">${values.title}</h4>
+        <img src=${values.image} alt="img" class="image">
+        <p>${values.description}</p>
+        <p class="category">${values.category}</p>
+        <p class="price">${values.price}</p>
+    </div>`
+    
+      });
+
+      document.getElementById("cards").innerHTML=data1;
+  })
+  .catch((err)=>{
+    console.log(err);
+  })
