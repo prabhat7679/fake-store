cb402421714da84d52d515f34ad149ddc9193a5e
prabhat kumar <prabhat.kumar.24.2@mountblue.tech>
2023-07-13 14:02:11 +0530

    modified login.html add login.js   implement with addEventListener

---
+++ login.js
@@ -0,0 +1,22 @@
+
+    form.addEventListener('submit',(e)=>{
+        
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const repeatPassword = document.getElementById("repeatPassword").value;
+         const tos = document.getElementById("tos").checked;
+    
+        e.preventDefault()
+       
+            if (firstName !== "" && lastName !== "" && email !== "" && password !== "" && repeatPassword !== "" && tos) {
+                
+                window.location.href="./index.html";
+            } else {
+                
+                alert("Please fill in all form fields and agree to the Terms of Service.");
+            }
+    })
+
+    
